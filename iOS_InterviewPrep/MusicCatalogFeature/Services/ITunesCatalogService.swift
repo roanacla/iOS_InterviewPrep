6754127d6c071f45c076b14b84b458d599cf6c76
Roger Navarro <roanacla@me.com>
2025-11-26 17:37:13 -0800

    Challenge 3 + Suggestions

--- iOS_InterviewPrep/MusicCatalogFeature/Services/ITunesCatalogService.swift
+++ iOS_InterviewPrep/MusicCatalogFeature/Services/ITunesCatalogService.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 enum MusicCatalogServiceError: Error {
+    case invalidSearchTerm
     case noDataReturned
     case noResponse
     case invalidURL
@@ -10,22 +11,25 @@ class ITunesCatalogService: MusicCatalogServiceProtocol {
     private enum EndPoint: String {
         case itunesSearchURL = "https://itunes.apple.com/search?media=music&entity=song&term="
     }
+    lazy var urlSession: URLSession = .shared
+    lazy var jsonDecoder = JSONDecoder()
     
     func search(term: String) async throws -> [CatalogItem] {
+        guard let term = term.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+            throw MusicCatalogServiceError.invalidSearchTerm
+        }
         let endPoint = EndPoint.itunesSearchURL.rawValue + term
         guard let url = URL(string: endPoint) else {
             throw MusicCatalogServiceError.invalidURL
         }
         
-        let urlSession = URLSession.shared
         let request = URLRequest(url: url)
         let (data, response) = try await urlSession.data(for: request)
         
-        guard let response = (response as? HTTPURLResponse)?.statusCode, (200...299).contains(response) else {
+        guard let response = (response as? HTTPURLResponse)?.statusCode, response == 200 else {
             throw MusicCatalogServiceError.noResponse
         }
         
-        let jsonDecoder = JSONDecoder()
         let iTunesSearchResponse = try jsonDecoder.decode(ITunesSearchResponse.self, from: data)
         
         return iTunesSearchResponse.results
